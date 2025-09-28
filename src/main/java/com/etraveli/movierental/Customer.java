b1a0a06b19514bb2de57b731220d1de1a9224646
vivek <vivek.dubeydeveloper@gmail.com>
2025-09-28 13:56:57 +0200

    Initial commit

---
+++ src/main/java/com/etraveli/movierental/Customer.java
@@ -0,0 +1,21 @@
+package com.etraveli.movierental;
+
+import java.util.List;
+
+public class Customer {
+    private String name;
+    private List<MovieRental> rentals;
+
+    public Customer(String name, List<MovieRental> rentals) {
+        this.name = name;
+        this.rentals = rentals;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<MovieRental> getRentals() {
+        return rentals;
+    }
+}
---
+++ src/main/java/com/etraveli/movierental/MovieRental.java
@@ -0,0 +1,19 @@
+package com.etraveli.movierental;
+
+public class MovieRental {
+    private String movieId;
+    private int days;
+
+    public MovieRental(String movieId, int days) {
+        this.movieId = movieId;
+        this.days = days;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public int getDays() {
+        return days;
+    }
+}
